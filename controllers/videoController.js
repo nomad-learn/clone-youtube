8ddfa7c7a364843ac9200c06334e580fe89ed9e2
ddamjengi <silluat11@gmail.com>
2019-12-20 15:28:34 +0900

    dotenv installing & db,init js settings

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,3 @@
-import { Videos } from "../db";
 import routes from "../routes";
 
 export const home = (req, res) =>
--- db.js
+++ db.js
@@ -1,50 +1,16 @@
-export const Videos = [
-  {
-    id: 1232141,
-    title: "video awesome",
-    description: "This is something I love",
-    view: 25,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 213142,
-      name: "Juno",
-      email: "Juno@Juno.com"
-    }
-  },
-  {
-    id: 4342342,
-    title: "video supuer",
-    description: "This is something I love",
-    view: 26,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 213142,
-      name: "Juno",
-      email: "Juno@Juno.com"
-    }
-  },
-  {
-    id: 63243252,
-    title: "video great",
-    description: "This is something I love",
-    view: 27,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 213142,
-      name: "Juno",
-      email: "Juno@Juno.com"
-    }
-  },
-  {
-    id: 3423515,
-    title: "video excerent",
-    description: "This is something I love",
-    view: 28,
-    videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-    creator: {
-      id: 213142,
-      name: "Juno",
-      email: "Juno@Juno.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`💢 Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
