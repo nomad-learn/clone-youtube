d757882fe790d3faf6aecaeb03ae94f77d044a62
ddamjengi <silluat11@gmail.com>
2019-12-24 01:44:51 +0900

    d

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const Videos = await Video.find({});
+    const Videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", Videos });
   } catch (error) {
     res.render("home", { pageTitle: "Home", Videos: [] });
