b3cbd4c0db8ac49543b62f3a1613384bb8497675
ddamjengi <silluat11@gmail.com>
2019-12-25 23:25:09 +0900

    webpack module setting

--- static/main.js
+++ static/main.js
@@ -81,14 +81,21 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = "./assets/js/main.js");
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports) {
+/******/ ({
 
+/***/ "./assets/js/main.js":
+/*!***************************!*\
+  !*** ./assets/js/main.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '../scss/styles.scss'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\r\n\n\n//# sourceURL=webpack:///./assets/js/main.js?");
 
 /***/ })
-/******/ ]);
+
+/******/ });
