738a7b84cf0d419e07236b0a480b5356b306367c
ddamjengi <silluat11@gmail.com>
2020-01-02 21:02:18 +0900

    m

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,16 +2,20 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
   changePassword,
-  users
+  users,
+  postEditProfile,
+  getEditProfile
 } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 
 userRouter.get(routes.userDetail(), onlyPrivate, userDetail);
